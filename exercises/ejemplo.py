c0f5c17521e175671b8b9ac9cd9513eae4c9654d
Andres Munoz <anmmunozsa@outlook.es>
2026-03-17 20:08:18 -0500

    ejercicio regresion

---
+++ exercises/ejemplo.py
@@ -0,0 +1,7 @@
+torres_gemelas = 20250911
+
+Hola_mundo = "Hola Mundo" # a la variable Hola_mundo le asigno una cadena de caracteres (string) delimitado por unas comillas
+print(Hola_mundo) 
+
+Hola_mundo2 = "Hola \n Mundo" # \n genera un salto de linea #REGEX # ENCODING
+print(Hola_mundo2)
